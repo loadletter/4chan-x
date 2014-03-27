fdeaddbcd557ba51a3870c9e635aa870d00fe883
loadletter <loadletter@users.noreply.github.com>
2014-03-27 23:42:16 +0100

    Fix #5
    Added a mutationobserverthing like in v3, this seems to fix the captcha not reloading
    
    v3:
    https://github.com/MayhemYDG/4chan-x/commit/825236a002f17d90ec44440ed66d47c1737deed7
    https://github.com/MayhemYDG/4chan-x/issues/1524

--- 4chan_x.user.js
+++ 4chan_x.user.js
@@ -2321,6 +2321,11 @@
           return _this.count(arr.length);
         });
         this.count($.get('captchas', []).length);
+        new MutationObserver(QR.captcha.load.bind(QR.captcha)).observe(this.challenge, {
+          childList: true,
+          subtree: true,
+          attributes: true
+        });
         return this.reload();
       },
       save: function() {
