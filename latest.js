6c8a54ef9818ff2db1d2e0f84f3dd5b3772e7b67
loadletter <loadletter@users.noreply.github.com>
2014-06-15 22:35:15 +0200

    Release 2.40.31

--- latest.js
+++ latest.js
@@ -1,1 +1,1 @@
-postMessage({version:'2.40.30'},'*')
+postMessage({version:'2.40.31'},'*')
--- updateheaders.sh
+++ updateheaders.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "\e[0;31mREMEMBER TO CHANGE version: @ LINE 5983 (before CSS) and TO TAG THE RELEASE and to have utils/config in .git/config\e[0m"
+echo -e "\e[0;31mREMEMBER TO CHANGE version: @ LINE 6008 (before CSS) and TO TAG THE RELEASE and to have utils/config in .git/config\e[0m"
 echo "======meta.js======="
 head -26 4chan_x.user.js | tee 4chan_x.meta.js
 
