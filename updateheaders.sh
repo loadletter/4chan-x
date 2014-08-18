82bee9d363902f794032c89a8df5272dcc443521
loadletter <loadletter@users.noreply.github.com>
2014-08-18 21:12:15 +0200

    updateheaders.sh: detect version line automatically

--- updateheaders.sh
+++ updateheaders.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-echo -e "\e[0;31mREMEMBER TO CHANGE version: @ LINE 6015 (before CSS) and TO TAG THE RELEASE and to have utils/config in .git/config\e[0m"
 echo "======meta.js======="
 head -26 4chan_x.user.js | tee 4chan_x.meta.js
 
 echo "=====latest.js======"
 vers=$(head -26 4chan_x.meta.js | grep "// @version" | awk '{print $3}')
+versline=$(grep -n "^    version: .$vers.," 4chan_x.user.js | cut -f1 -d':')
 echo -n "postMessage({version:'"$vers"'},'*')" | tee latest.js
 echo
+echo -e "\e[0;31mREMEMBER TO CHANGE version @ LINE: $versline (before CSS) and TO TAG THE RELEASE and to have utils/config in .git/config\e[0m"
