a5496cf5cd45009561ebb51520b13e6fac1fb931
loadletter <loadletter@users.noreply.github.com>
2016-01-26 20:58:17 +0100

    Release 2.40.62

--- 4chan_x.meta.js
+++ 4chan_x.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           4chan x
-// @version        2.40.61
+// @version        2.40.62
 // @namespace      aeosynth
 // @description    Adds various features.
 // @copyright      2009-2011 James Campos <james.r.campos@gmail.com>
--- latest.js
+++ latest.js
@@ -1,1 +1,1 @@
-postMessage({version:'2.40.61'},'*')
+postMessage({version:'2.40.62'},'*')
