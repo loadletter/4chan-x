035dbea11fca125a7a20c66fb3416efd4f737428
loadletter <loadletter@users.noreply.github.com>
2017-05-14 23:34:23 +0200

    Release 2.40.73

--- 4chan_x.user.js
+++ 4chan_x.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           4chan x
-// @version        2.40.72
+// @version        2.40.73
 // @namespace      aeosynth
 // @description    Adds various features.
 // @copyright      2009-2011 James Campos <james.r.campos@gmail.com>
@@ -6343,7 +6343,7 @@
       return $.globalEval(("(" + code + ")()").replace('_id_', bq.id));
     },
     namespace: '4chan_x.',
-    version: '2.40.72',
+    version: '2.40.73',
     callbacks: [],
     css: '\
 /* dialog styling */\
--- latest.js
+++ latest.js
@@ -1,1 +1,1 @@
-postMessage({version:'2.40.72'},'*')
+postMessage({version:'2.40.73'},'*')
